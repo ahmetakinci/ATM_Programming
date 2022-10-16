8f441fb9e1610056ebdece1c606027516f1f162d
Ahmet Arif Akıncı <ahmetarifaknci@gmail.com>
2022-10-16 21:00:36 +0300

    First ATM programming commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        ATM atm = new ATM();
+        Hesap hesap = new Hesap("Ahmet Arif","12345",2000);
+        atm.calis(hesap);
+        System.out.println("Programdan Çıkılıyor...");
+
+    }
+}
